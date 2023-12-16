3058302fd5363926668d2d0cc516a401bb29cc39
tan <1356026577@qq.com>
2023-12-16 16:44:10 +0800

    [字符串反转]去掉多余空格

--- CharacterInversion/Inversion.c
+++ CharacterInversion/Inversion.c
@@ -3,8 +3,10 @@
 
 void killPlace(char* arr)
 {   
+    
     int poss = 1;
     int idx = 1;
+
     while(arr[idx] != '\0')
     {   /* 以下这三个if、elseif、else不可用while替代 */
         if(arr[idx] == ' ' && arr[idx - 1] != ' ')
@@ -18,18 +20,19 @@ void killPlace(char* arr)
         else
         {
             arr[poss++] = arr[idx++];
-        }
-        
+        }   
     }
+
     arr[poss] = '\0';/* 结尾加一个'/0' */
     printf("去空格后的字符串为： %s\n", arr);
-    //return poss;
 }   
 
 void changeWord(char *str)
 {
+
     int start = 0;
     int end = strlen(str) - 1;
+
     while(start < end)
     {
         char temp = str[start];
@@ -39,18 +42,22 @@ void changeWord(char *str)
         end--;
     }
     int idx = 0;
+
     while(str[idx] != '\0')
     {
+
         while(str[idx] == ' ' && str[idx] != '\0')
         {
             idx++;
         }
         int wordStart = idx;
+
         while(str[idx] != ' '&& str[idx] != '\0')
         {
             idx++;
         }
         int wordEnd = idx-1;
+
         while(wordStart < wordEnd)
         {
             char temp = str[wordStart];
@@ -59,16 +66,23 @@ void changeWord(char *str)
             wordStart++;
             wordEnd--; 
         }
+
     }
 }
 
 int main()
 {
     char str[100];
+
     printf("输入: ");
+
     gets(str);
+
     killPlace(str);
+
     changeWord(str);
+
     printf("fanzhuan: %s\n",str);
+
     return 0;
 }
